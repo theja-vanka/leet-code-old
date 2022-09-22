45d1768cf9ff48a9bd2bc30ab5a82bf7c9ef4a67
Krishnatheja Vanka <theja.vanka@gmail.com>
2022-09-22 10:59:59 +0530

    Time: 209 ms (8.74%), Space: 19.1 MB (83.27%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         vector<int>::iterator it;
-        vector<int> zeroLocation = {};
-        
-        for(int i=0; i<nums.size(); i++){
-             if(nums[i] == 0){
-                 zeroLocation.push_back(i);
-             }
-        }
-        for(int i=zeroLocation.size()-1; i>=0; i--){
-            it=nums.begin()+zeroLocation[i];
-            nums.erase(it);
-            nums.push_back(0);
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] == 0){
+                it=nums.begin()+i;
+                nums.erase(it);
+                nums.push_back(0);    
+            } 
         }
     }
 };
