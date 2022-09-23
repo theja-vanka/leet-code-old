884c372eead19272f41f882f655bca537cadf62e
Krishnatheja Vanka <theja.vanka@gmail.com>
2022-09-23 23:04:41 +0530

    Time: 7 ms (17.92%), Space: 8.3 MB (23.60%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=nums.size();
+        int low=0,mid=0,high=l-1;
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0:swap(nums[low++],nums[mid++]); break;
+                case 1:mid++; break;
+                case 2:swap(nums[mid],nums[high--]); break;
+            }
+        }   
+    }
+};
