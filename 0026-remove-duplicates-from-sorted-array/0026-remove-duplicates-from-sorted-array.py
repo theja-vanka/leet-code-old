c384cc07749aea286d6e554d1cebe70cc1b2ebc5
Krishnatheja Vanka <theja.vanka@gmail.com>
2023-10-19 12:13:05 +0530

    Time: 90 ms (35.7%), Space: 17.9 MB (38.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,9 +5,7 @@ class Solution:
         current_value = nums[0]
 
         for i in range(1,len(nums), 1):
-            if current_value == nums[i]:
-                continue
-            else:
+            if current_value != nums[i]:
                 nums[pointer], current_value = nums[i], nums[i]
                 pointer += 1
         
