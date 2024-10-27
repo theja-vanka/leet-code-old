6a1d844ba5f759deb9836c94be48fee9fc006f0d
Krishnatheja Vanka <theja.vanka@gmail.com>
2024-10-28 01:10:47 +0530

    Time: 1 ms (80.38%), Space: 16.8 MB (13.6%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,46 +6,37 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        carry: int = 0
-        base: int = 0
+        carry = 0
+        base = 0
 
-        return_l: Optional[ListNode] = ListNode()
-        sum_l: Optional[ListNode] = return_l
-
-        while l1 != None and l2 != None:
-            base = l1.val + l2.val + carry
-            carry = base//10
-            base = base % 10
-
-            return_l.next = ListNode(base)
-
-            return_l = return_l.next
+        base = l1.val + l2.val
+        carry, base = divmod(base, 10)
+        suml = ListNode(base)
+        returnl = suml
+        l1 = l1.next
+        l2 = l2.next
+        while l1 is not None and l2 is not None:
+            base = l1.val + l2.val
+            carry, base = divmod(carry + base, 10)
+            suml.next = ListNode(base)
             l1 = l1.next
             l2 = l2.next
+            suml = suml.next
         
-        while l1 != None:
-            base = l1.val + carry
-            carry = base//10
-            base = base % 10
-
-            return_l.next = ListNode(base)
-
-            return_l = return_l.next
+        while l1 is not None:
+            base = l1.val
+            carry, base = divmod(carry + base, 10)
+            suml.next = ListNode(base)
+            suml = suml.next
             l1 = l1.next
         
-        while l2 != None:
-            base = l2.val + carry
-            carry = base//10
-            base = base % 10
-
-            return_l.next = ListNode(base)
-
-            return_l = return_l.next
+        while l2 is not None:
+            base = l2.val
+            carry, base = divmod(carry + base, 10)
+            suml.next = ListNode(base)
+            suml = suml.next
             l2 = l2.next
-        
-        if carry > 0:
-            return_l.next = ListNode(carry)
-        
-        return sum_l.next
-
-        
+            
+        if carry != 0:
+            suml.next = ListNode(carry)
+        return returnl
